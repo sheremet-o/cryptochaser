cef23e8e13d4d7ded735e501a57263cf18bf8537
Oksana Sheremet <sheremet5631@gmail.com>
2024-08-23 09:35:14 +0300

    Added NewCoin func

--- cryptochaser/cryptochaser.go
+++ cryptochaser/cryptochaser.go
@@ -1,6 +1,7 @@
 package cryptochaser
 
 import (
+	"errors"
 	"time"
 )
 
@@ -8,4 +9,19 @@ type Coin struct {
 	Title     string
 	Cost      float64
 	CreatedAt time.Time
+}
+
+func NewCoin(title string, cost float64) (*Coin, error) {
+	if title == "" {
+		return nil, errors.New("название валюты не может быть пустым")
+	}
+	if cost <= 0 {
+		return nil, errors.New("курс валюты должен быть положительным числом")
+	}
+
+	return &Coin{
+		Title:     title,
+		Cost:      cost,
+		CreatedAt: time.Now(),
+	}, nil
 }
